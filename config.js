7dac3fd8f90824b8544179c35e13bdf8ca01eb30
boshptw-stack <boshptw@gmail.com>
2026-05-03 09:41:55 +0800

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-/* ============================================
-   BOSH CMS — Configuration
-   ============================================ */
-const CONFIG = {
-  SHEET_ID: '1tfcQ64r5-WIdRfrBp_N07AGNYY8R67DSaS9DbLRWKgw',
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbzmpyJyKnLbm481LWEoT_KEhjO42_TksJDdErnNf9VvINrIKW2YOM9CLqP49XOQ53xdWw/exec',
-  BASE_PATH: '/cms/',
-  VERSION: '2.0.0'
-};
